1199883dacf45c5f3b897a5a7479ab01990afee8
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-04-05 23:42:37 +0330

    feat: better install

--- install.sh
+++ install.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
+
 # Create $HOME/.local/bin if it doesn't exist
 mkdir -p $HOME/.local/bin
 
 if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-    echo "Add $HOME\/.local/bin to the \$PATH"
-    exit 1
+    echo "Adding $HOME/.local/bin to the \$PATH"
+    export PATH=$PATH:$HOME/.local/bin
+    if [ "$SHELL" = "/bin/bash" ]; then
+        echo "export PATH=\"\$PATH:$HOME/.local/bin\"" >> ~/.bashrc
+        source ~/.bashrc
+    elif [ "$SHELL" = "/usr/bin/fish" ]; then
+        echo "set -x PATH $HOME/.local/bin \$PATH" >> ~/.config/fish/config.fish
+        source ~/.config/fish/config.fish
+    elif [ "$SHELL" = "/usr/bin/zsh" ]; then
+        echo "export PATH=\"\$PATH:$HOME/.local/bin\"" >> ~/.zshrc
+        source ~/.zshrc
+    else
+        echo "Unsupported shell: $SHELL"
+        exit 1
+    fi
 fi
+
 # Copy the script to $HOME/.local/bin
 cp poll_exec $HOME/.local/bin
 
-# Check which shell the user is using and modify the appropriate configuration file
-if [ "$SHELL" = "/bin/bash" ]; then
-    echo "export PATH=\"\$PATH:$HOME/.local/bin\"" >> ~/.bashrc
-    echo "Installation complete."
-    echo "To use the script, run the following command:"
-    echo "poll_exec <directory> <command>"
-elif [ "$SHELL" = "/usr/bin/fish" ]; then
-    echo "set -x PATH $HOME/.local/bin \$PATH" >> ~/.config/fish/config.fish
-    echo "Installation complete."
-    echo "To use the script, run the following command:"
-    echo "poll_exec <directory> <command>"
-elif [ "$SHELL" = "/usr/bin/zsh" ]; then
-    echo "export PATH=\"\$PATH:$HOME/.local/bin\"" >> ~/.zshrc
-    echo "Installation complete."
-    echo "To use the script, run the following command:"
-    echo "poll_exec <directory> <command>"
-else
-    echo "Unsupported shell: $SHELL"
-fi
-
+echo "Installation complete."
+echo "To use the script, run the following command:"
+echo "poll_exec <directory> <command>"
