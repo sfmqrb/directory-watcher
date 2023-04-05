a9baa183d2d613dce70ec98911eb2699fde488ce
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-04-05 23:03:35 +0330

    feat: better true

--- poll_change.sh
+++ poll_change.sh
@@ -2,7 +2,7 @@ DIR_TO_WATCH=${1}
 COMMAND=${2}
  
 trap "echo Exited!; exit;" SIGINT SIGTERM
-while [[ 1=1 ]]
+while : 
 do
   watch --chgexit -n 1 "ls -ltr --recursive --full-time ${DIR_TO_WATCH} | sha256sum" && ${COMMAND}
   sleep 1
